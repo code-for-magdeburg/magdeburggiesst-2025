1bb153fc83e6176be2a3b839d26f39aaa0c020cd
Jens Winter-Huebenthal <jens.winter-huebenthal@bridgefield.de>
2025-03-27 22:34:15 +0100

    hide update pumps link
    Closes #14

--- src/components/pumps/pump-tooltip.tsx
+++ src/components/pumps/pump-tooltip.tsx
@@ -45,7 +45,11 @@ export const PumpTooltip: React.FC<PumpTooltipProps> = ({ pump }) => {
 					<div className="">{pump.lastCheck}</div>
 				</div>
 			</div>
-			<div className="text-sm pt-2 border-t-2 ">
+			{
+				/* Pump update link hidden.
+				Refer to this issue: https://github.com/technologiestiftung/giessdenkiez-de/issues/999 */
+			}
+			<div className="text-sm pt-2 border-t-2 hidden">
 				<a
 					className="underline"
 					target="_blank"
